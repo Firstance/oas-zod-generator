5128f76f80a802a8fa789aaad31548823ba9be38
gabriele.lana <gabriele.lana@gmail.com>
2023-03-04 16:12:14 +0100

    Make it runnunable

---
+++ dist/src/types.d.ts
@@ -0,0 +1,10 @@
+export type Request = {
+    pathParameters: Record<string, string | undefined>;
+    queryParameters: Record<string, string | undefined>;
+    body?: any;
+};
+export type Response = {
+    statusCode: number;
+    contentType: string;
+    body: any;
+};
